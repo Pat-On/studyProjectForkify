3be88ba1a005acc0e045abf46df4d4c272b5d4aa
PatOn <mahomed2278@gmail.com>
2021-02-02 19:35:54 +0000

    Publisher and subscriber pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -52,6 +52,7 @@ controlRecipes();
 fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
 
 
-['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes))
-// window.addEventListener('hashchange', showRecipe);
-// window.addEventListener('load', showRecipe);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+}
+init();
