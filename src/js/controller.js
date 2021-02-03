d98e90adf3442505101451df8f013de6f5870e27
PatOn <mahomed2278@gmail.com>
2021-02-03 10:13:12 +0000

    MVC deeper implementation plus resultsView.js implemented

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,11 +1,17 @@
 // import { render } from 'sass'; // not my code 'Parcel"? check more later in google. <-reason of error
 import * as model from './model.js';
 import recipeView from "./views/recipeView.js";
-import searchView from "./views/searchView.js"
+import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+//this is for parcel
+if (module.hot) {
+  module.hot.accept();
+};
 
 
 const recipeContainer = document.querySelector('.recipe');
@@ -54,6 +60,7 @@ controlRecipes();
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     // 1) Get search query
     const query = searchView.getQuery();
     // console.log(query)
@@ -63,7 +70,8 @@ const controlSearchResults = async function () {
     //2) Load search
     await model.loadSearchResults(query);
     //3) Render results
-    console.log(model.state.search.results);
+    // console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err)
   }
